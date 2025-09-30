1fdc8488ebf11d17a1ee371e1569d39e9f06a67c
thibauttaglialatela <thibauttaglialatela@gmail.com>
2025-09-30 16:36:55 +0200

    📦 NEW:  install and configure prettier + eslint

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -26,4 +27,5 @@ export default defineConfig([
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
   },
+  eslintConfigPrettier
 ])
