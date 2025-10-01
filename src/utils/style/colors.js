95ab55d15a01e954dc7fe9ac4907df41b5a6bc58
thibauttaglialatela <thibauttaglialatela@gmail.com>
2025-10-01 11:01:49 +0200

    👌 IMPROVE: mettre en place le théme global

---
+++ src/utils/style/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+  primary: '#005A9C',
+  secondary: '#E27A3F',
+  black: '#1E1E1E',
+  white: '#F7F7F7',
+  blackTransparent: '#202828',
+  error: '#FF5863',
+}
+
+export default colors
