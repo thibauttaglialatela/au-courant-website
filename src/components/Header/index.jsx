97998d88f2fc9c8b70cc30c709692a023256ccee
thibauttaglialatela <thibauttaglialatela@gmail.com>
2025-10-20 16:43:32 +0200

    feat: :construction: create the header component
    create a header component with image and heading 1

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,47 @@
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import colors from '../../utils/style/colors'
+
+const StyledHeader = styled.header`
+  display: flex;
+  flex-direction: column;
+`
+const HeaderImage = styled.img`
+  width: 100%;
+  height: auto;
+
+  @media screen and (min-width: 426px) {
+    aspect-ratio: 16/5;
+    object-fit: cover;
+    object-position: 50% 10%;
+  }
+`
+
+const StyledHeaderTitle = styled.h1`
+  text-align: center;
+  color: ${colors.primary};
+  margin-top: 2.5rem;
+
+  @media screen and (min-width: 426px) {
+    margin-top: 4rem;
+  }
+`
+const Header = ({ title, imageSrc, imageAlt }) => {
+  return (
+    <StyledHeader>
+      {imageSrc && (
+        <HeaderImage src={imageSrc} alt={imageAlt || ''} loading="lazy" />
+      )}
+
+      <StyledHeaderTitle>{title}</StyledHeaderTitle>
+    </StyledHeader>
+  )
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+  imageSrc: PropTypes.string,
+  imageAlt: PropTypes.string,
+}
+
+export default Header
